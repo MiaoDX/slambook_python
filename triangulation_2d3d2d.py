b94ff3e00b0aff31c66d211817704e9fd686e6cd
miaodx <miaodx@hotmail.com>
2017-08-15 17:06:46 +0800

    Regular update.

--- triangulation_2d3d2d.py
+++ triangulation_2d3d2d.py
@@ -7,11 +7,10 @@
 4.prune out some points
 5.use solvePnP to re-calc the R,t
 
-
 """
 import cv2
 from matplotlib import pyplot as plt
-
+import numpy as np
 
 def find_keypoints_and_description(img):
     # Initiate STAR detector
@@ -27,10 +26,15 @@ def find_keypoints_and_description(img):
 
     print("KP points:{}".format(len(kpp)))
 
+    if DEBUG:
+        print("first 10 key points:")
+        for kpi in kp[:10]:
+            print("{}".format(cv2.KeyPoint_convert([kpi])))
+
     """
-    kp1, des1 = orb.detectAndCompute(img, None)
     # Make sure the key-points are the same
     
+    kp1, des1 = orb.detectAndCompute(img, None)    
     for kpi, kppi, kp1i in zip(kp, kpp, kp1):
         # print("{},{},{}\n".format(kpi, kppi,cv2.KeyPoint_overlap(kpi, kppi)))
         print("{},{},{}".format(cv2.KeyPoint_convert([kpi]), cv2.KeyPoint_convert([kppi]),  cv2.KeyPoint_convert([kp1i])))
@@ -50,40 +54,198 @@ def find_keypoints_and_description(img):
 
 
 
-
 def find_feature_matches_from_keypoints_and_descriptors(img1, kp1, des1, img2, kp2, des2):
 
     # create BFMatcher object
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING) # to make sure we are the same as the C++ version
 
     # Match descriptors.
     matches = bf.match(des1, des2)
 
     # Sort them in the order of their distance.
-    matches = sorted(matches, key=lambda x: x.distance)
+    matches_sorted = sorted(matches, key=lambda x: x.distance)
 
-    # Draw first 10 matches.
-    # img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=2)
-
-    for m in matches[:5]:
-        print("distance in match:{}".format(m.distance))
-
+    print("Max distance:{}, min distance:{}".format(matches_sorted[-1].distance, matches_sorted[0].distance))
     # 当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
-    prune_dis = max(matches[0].distance, 30.0)
+    prune_dis = max(matches_sorted[0].distance, 30.0)
+
     matches = list(filter(lambda x: x.distance <= prune_dis, matches))
 
     print("We found {} pairs of points in total".format(len(matches)))
 
-    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
+    #img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
+    #plt.imshow(img3), plt.show()
 
-    plt.imshow(img3), plt.show()
-    pass
 
+
+    return matches
+
+
+def essentialFromFundamental(F, K1, K2):
+    """
+    Normally, we only need to set K1 and K2 to the same one
+    """
+    return K2.T.dot(F).dot(K1)
+
+def find_F_E_R_t(kp1, kp2, matches):
+    """
+    [ref](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_epipolar_geometry/py_epipolar_geometry.html)
+    :param kp1:
+    :param kp2:
+    :param matches:
+    :return:
+    """
+
+    pts1 = []
+    pts2 = []
+    for m in matches:
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+
+    #  Let’s find the Fundamental Matrix
+    #pts1 = np.int32(pts1)
+    #pts2 = np.int32(pts2)
+    pts1 = np.array(pts1)
+    pts2 = np.array(pts2)
+
+    """
+    for i in range(len(pts1)):
+        print("i:{}".format(i))
+        print("p1:{}".format(pts1[i]))
+        print("p2:{}".format(pts2[i]))
+    """
+
+    # F, mask_F = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+    F, mask_F = cv2.findFundamentalMat(pts1, pts2, method = cv2.FM_8POINT)
+
+    # We select only inlier points
+    pts1_F = pts1[mask_F.ravel() == 1]
+    pts2_F = pts2[mask_F.ravel() == 1]
+    print("inner points in findFundamentalMat num:{}".format(len(pts1_F)))
+    print("F:\n{}".format(F))
+
+
+    K = np.array([[520.9, 0, 325.1], [0, 521.0, 249.7], [0, 0, 1]])
+
+
+    E, mask_E = cv2.findEssentialMat(pts1, pts2, cameraMatrix=K)
+    # We select only inlier points
+    pts1_E = pts1[mask_E.ravel() == 1]
+    pts2_E = pts2[mask_E.ravel() == 1]
+    print("inner points in findEssentialMat num:{}".format(len(pts1_E)))
+    print("E:\n{}".format(E))
+
+    RE1, RE2, tE = cv2.decomposeEssentialMat(E) # there will be two answers for R, and four answers in total: R1 t, R1 -t, R2 t, R2 -t
+    print("R t from decomposeEssentialMat")
+    print("RE1:{}".format(RE1))
+    r1, _ = cv2.Rodrigues(RE1)
+    print("r:\n{}".format(r1))
+    print("RE2:{}".format(RE2))
+    r2, _ = cv2.Rodrigues(RE2)
+    print("r:\n{}".format(r2))
+    print("tE:{}".format(tE))
+
+    # it should be the same as the recoverPose, however, it is not. It seem that the check_solutions is somewhat wrong
+    import utils
+    R_checked, t_checked = utils.check_solutions(pts1_E, pts2_E, K, RE1, RE2, tE)
+    print("R t from decomposeEssentialMat and checked")
+    print("R_checked:{}".format(R_checked))
+    print("t_checked:{}".format(t_checked))
+
+    E_FF = essentialFromFundamental(F, K, K) # it should be the same, however, it is not :(
+    print("E from fundamental:\n{}".format(E_FF))
+    """
+    RE1_FF, RE2_FF, tE_FF = cv2.decomposeEssentialMat(E_FF)  # there will be two answers for R
+    print("R t from decomposeEssentialMat, which E from F")
+    print("RE1_FF:{}".format(RE1_FF))
+    r1_FF, _ = cv2.Rodrigues(RE1_FF)
+    print("r:\n{}".format(r1_FF))
+    print("RE2_FF:{}".format(RE2_FF))
+    r2_FF, _ = cv2.Rodrigues(RE2_FF)
+    print("r_FF:\n{}".format(r2_FF))
+    print("tE_FF:{}".format(tE_FF))
+    """
+
+
+    H, mask_H = cv2.findHomography(pts1, pts2, method=cv2.RANSAC, ransacReprojThreshold=3)
+    # We select only inlier points
+    pts1_H = pts1[mask_H.ravel() == 1]
+    pts2_H = pts2[mask_H.ravel() == 1]
+    print("inner points in findHomography num:{}".format(len(pts1_H)))
+    print("H:\n{}".format(H))
+
+    _, RH, tH, _ = cv2.decomposeHomographyMat(H, K) # it will have four answers, we need to find which is better
+    print("R t from decomposeHomographyMat")
+    print("RH:{}".format(RH))
+    print("tH:{}".format(tH))
+
+
+    _, R, t, mask_rc = cv2.recoverPose(E, pts1, pts2, K) # this can have the determiate results, so we choose it
+    # We select only inlier points
+    print("inner points in recoverPose num:{}".format(len(mask_rc)))
+    pts1_rc = pts1[mask_rc.ravel() == 1]
+    pts2_rc = pts2[mask_rc.ravel() == 1]
+
+    print("R t from recoverPose")
+    r, _ = cv2.Rodrigues(R)
+    print("R:\n{}".format(R))
+    print("r:\n{}".format(r))
+    print("t:\n{}".format(t))
+
+    # find_F_E_R_t
+    return F, E, R, t, pts1_F, pts2_F, pts1_E, pts2_E
+
+
+
+def drawlines(img1,img2,lines,pts1,pts2):
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    print("img1.shape:{}".format(img1.shape))
+    r,c = img1.shape[:2] # it can be gray or RGB
+    #img1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
+    #img2 = cv2.cvtColor(img2,cv2.COLOR_GRAY2BGR)
+    for r,pt1,pt2 in zip(lines,pts1,pts2):
+        color = tuple(np.random.randint(0,255,3).tolist())
+        x0,y0 = map(int, [0, -r[2]/r[1] ])
+        x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+        img1 = cv2.line(img1, (x0,y0), (x1,y1), color,1)
+        img1 = cv2.circle(img1,tuple(pt1),5,color,-1)
+        img2 = cv2.circle(img2,tuple(pt2),5,color,-1)
+    return img1,img2
+
+def draw_epilines_from_F(img1, img2, pts1, pts2, F):
+    # Find epilines corresponding to points in right image (second image) and
+    # drawing its lines on left image
+
+    # make them to int so that we can draw on them
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+
+    lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
+    lines1 = lines1.reshape(-1,3)
+    img5,img6 = drawlines(img1,img2,lines1,pts1,pts2)
+
+    # Find epilines corresponding to points in left image (first image) and
+    # drawing its lines on right image
+    lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
+    lines2 = lines2.reshape(-1,3)
+    img3,img4 = drawlines(img2,img1,lines2,pts2,pts1)
+
+    plt.subplot(121),plt.imshow(img5)
+    plt.subplot(122),plt.imshow(img3)
+    plt.show()
 
 if __name__ == '__main__':
     base_dir = 'H:/projects/SLAM/slambook/ch7/'
     im1_file = base_dir+'1.png'
     im2_file = base_dir + '2.png'
+
+    DEBUG = True
+
+    if DEBUG:
+        print("HHHHH")
+
 
     im1 = cv2.imread(im1_file)
     im2 = cv2.imread(im2_file)
@@ -92,4 +254,47 @@ if __name__ == '__main__':
     kp1, des1 = find_keypoints_and_description(im1)
     kp2, des2 = find_keypoints_and_description(im2)
 
-    find_feature_matches_from_keypoints_and_descriptors(im1, kp1, des1, im2, kp2, des2)
+    matches = find_feature_matches_from_keypoints_and_descriptors(im1, kp1, des1, im2, kp2, des2)
+
+    F, E, R, t, pts1_F, pts2_F, pts1_E, pts2_E = find_F_E_R_t(kp1, kp2, matches)
+
+    draw_epilines_from_F(im1, im2, pts1_F, pts2_F, F)
+
+
+
+"""
+SOME NOTES:
+1. image color and corresponding difference, `im1 = cv2.imread(im1_file)` and `im1 = cv2.imread(im1_file, 0)`
+2. inner points in findFundamentalMat, when it is not the same as the input num?
+3. when we calc Fundamental Matrix
+    pts1 = np.int32(pts1) and pts1 = np.array(pts1) will lead to very different answers, so, which is better??
+4. Matrix manipulation in C++ and python
+    [Opencv中Mat矩阵相乘——点乘、dot、mul运算详解](http://blog.csdn.net/dcrmg/article/details/52404580
+        
+    arr = [[1,2,3],[4,5,6]]
+    a = np.array(arr)
+    b = np.array(arr)
+    
+    C++     python
+    *       dot
+    a*b.t()     a.dot(b.T) or np.dot(a,b.T)
+    
+    dot     * -> sum
+    a.dot(b)    (a*b).sum()
+    
+    mul     *
+    a.mul(b)    a*b
+    
+    So, C++: E = K2.t () * F * K1;
+    python:  E = K2.T.dot(F).dot(K1)
+    
+5. The corresponding relationship between F,E and H
+
+6. [recoverPose](http://docs.opencv.org/3.2.0/d9/d0c/group__calib3d.html)
+
+    This function decomposes an essential matrix using decomposeEssentialMat and then verifies possible pose hypotheses by doing cheirality check. 
+    The cheirality check basically means that the triangulated 3D points should have positive depth. Some details can be found in [119] .
+    
+7. check_solutions(fp, sp, K, R1, R2, t):
+    should have same result as recoverPose, but without good luck            
+"""
---
+++ utils.py
@@ -0,0 +1,94 @@
+"""
+Copy from [calculate_homography](https://github.com/kevin-george/cv_tools/blob/master/calculate_homography.py)
+
+The reference can be found at [](http://igt.ip.uca.fr/~ab/Classes/DIKU-3DCV2/Handouts/Lecture16.pdf)
+"""
+
+import numpy as np
+import cv2
+
+def check_solutions(fp, sp, K, R1, R2, t):
+    """Using cv2.triangulatePoints to calculate position
+    in real-world co-ordinate system. If z value for both
+    points are positive, then that is our solution.
+    More details can be found in the paper,
+    David Nister, An efficient solution to the five-point relative pose problem.
+    TODO: Need to take care of points that are far away
+    Args:
+        fp: Set of matched points from first image
+        sp: Set of matched points from second image
+        K: Calibration matrix
+        R1: First rotation matrix
+        R2: Second rotation matrix
+        t: Translation vector
+    Returns:
+        R: Correct rotation matrix
+    """
+    dist = 50.0
+    P0 = np.eye(3,4)
+    P1 = np.dot(K, np.concatenate((R1, t.reshape(3,1)), axis=1))
+    Q = cv2.triangulatePoints(P0, P1, fp.T, sp.T)
+    mask1 = (Q[2, :] * Q[3, :]) > 0
+    Q[0, :] /= Q[3, :]
+    Q[1, :] /= Q[3, :]
+    Q[2, :] /= Q[3, :]
+    Q[3, :] /= Q[3, :]
+    mask1 &= (Q[2,:] < dist)
+    Q = np.dot(P1, Q)
+    mask1 &= (Q[2,:] > 0)
+    mask1 &= (Q[2,:] < dist)
+
+    P2 = np.dot(K, np.concatenate((R2, t.reshape(3, 1)), axis=1))
+    Q = cv2.triangulatePoints(P0, P2, fp.T, sp.T)
+    mask2 = (Q[2, :] * Q[3, :]) > 0
+    Q[0, :] /= Q[3, :]
+    Q[1, :] /= Q[3, :]
+    Q[2, :] /= Q[3, :]
+    Q[3, :] /= Q[3, :]
+    mask2 &= (Q[2, :] < dist)
+    Q = np.dot(P1, Q)
+    mask2 &= (Q[2, :] > 0)
+    mask2 &= (Q[2, :] < dist)
+
+    P3 = np.dot(K, np.concatenate((R1, -t.reshape(3, 1)), axis=1))
+    Q = cv2.triangulatePoints(P0, P3, fp.T, sp.T)
+    mask3 = (Q[2, :] * Q[3, :]) > 0
+    Q[0, :] /= Q[3, :]
+    Q[1, :] /= Q[3, :]
+    Q[2, :] /= Q[3, :]
+    Q[3, :] /= Q[3, :]
+    mask3 &= (Q[2, :] < dist)
+    Q = np.dot(P1, Q)
+    mask3 &= (Q[2, :] > 0)
+    mask3 &= (Q[2, :] < dist)
+
+    P4 = np.dot(K, np.concatenate((R2, -t.reshape(3, 1)), axis=1))
+    Q = cv2.triangulatePoints(P0, P4, fp.T, sp.T)
+    mask4 = (Q[2, :] * Q[3, :]) > 0
+    Q[0, :] /= Q[3, :]
+    Q[1, :] /= Q[3, :]
+    Q[2, :] /= Q[3, :]
+    Q[3, :] /= Q[3, :]
+    mask4 &= (Q[2, :] < dist)
+    Q = np.dot(P1, Q)
+    mask4 &= (Q[2, :] > 0)
+    mask4 &= (Q[2, :] < dist)
+
+    good1 = np.count_nonzero(mask1)
+    good2 = np.count_nonzero(mask2)
+    good3 = np.count_nonzero(mask3)
+    good4 = np.count_nonzero(mask4)
+
+    print("The four good:{},{},{},{}".format(good1, good2, good3, good4))
+
+    max_count = max(good1, good2, good3, good4)
+    if max_count == good1:
+        return R1, t
+    elif max_count == good2:
+        return R2, t
+    elif max_count == good3:
+        return R1, -t
+    elif max_count == good4:
+        return R2, -t
+
+    return None
